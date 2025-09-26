964bb2dee99dabb9d3b4ddc1e165cff099c1ad1b
TienNguyen3711 <s224882599@deakin.edu.au>
2025-09-27 09:27:30 +1000

    Update test_model.py

--- test/test_model.py
+++ test/test_model.py
@@ -1,12 +1,17 @@
 import joblib
 import json
 import numpy as np
+import pandas as pd
 
 
 def test_model_can_predict():
     model = joblib.load("model/model.pkl")
     cols = json.load(open("model/columns.json"))
-    x = np.zeros(len(cols))
-    y = model.predict([x])[0]
+
+    # Tạo dataframe 1 hàng, với toàn bộ features = 0
+    x = pd.DataFrame([np.zeros(len(cols))], columns=cols)
+
+    y = model.predict(x)[0]
+
     assert isinstance(float(y), float)
     assert y >= 0
